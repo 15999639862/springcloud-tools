fcb53287ad140fb0ba5234b9b3ba25c4d3d3af85
15999639862 <cwh0008@163.com>
2019-11-15 00:48:15 +0800

    init

---
+++ user_service/src/main/java/com/cwh/user/App.java
@@ -0,0 +1,13 @@
+package com.cwh.user;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
