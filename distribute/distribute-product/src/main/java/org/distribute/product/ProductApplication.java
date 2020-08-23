0ef9a8fcca38cda9b77f1930fe30181debf12655
15999639862 <cwh0008@163.com>
2020-08-24 02:42:58 +0800

    seata

--- distribute/distribute-product/src/main/java/org/distribute/product/ProductApplication.java
+++ distribute/distribute-product/src/main/java/org/distribute/product/ProductApplication.java
@@ -2,8 +2,11 @@ package org.distribute.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.FeignClient;
 
-
+@FeignClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductApplication {
 	public static void main(String[] args) {
